c6a800d1f49e027894b1dc620885a00e0a515604
Bish311 <bishwayan.24bcs10200@sst.scaler.com>
2026-05-02 01:00:11 +0530

    scaffold: maven project with spring boot WAR packaging

---
+++ src/main/java/dev/bish/library_crud/ServletInitializer.java
@@ -0,0 +1,13 @@
+package dev.bish.library_crud;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LibraryCrudApplication.class);
+	}
+
+}
